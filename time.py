89e11748583f716467397e1a029194b67713efd2
samy-belazzoug <samy.belazzoug@pm.me>
2026-08-01 17:22:14 +0200

    Formattage Temps fait.

---
+++ time.py
@@ -0,0 +1,10 @@
+import time
+def formattage_temps(heure,minute,seconde)->str:
+    return f'{heure}:{minute}:{seconde}'
+
+def mise_a_jour_temps(heure,minute,seconde)->str:
+    time.sleep(1)
+    seconde
+def afficher_temps(heure,minute,seconde):
+    while True:
+        pass
