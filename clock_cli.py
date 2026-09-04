68c2be9192719da928e7e19d4c27371f301c9a17
samy-belazzoug <samy.belazzoug@pm.me>
2026-09-04 23:55:25 +0200

    Better command input management of am/pm, integrating it in --time argument

--- clock_cli.py
+++ clock_cli.py
@@ -3,19 +3,18 @@ import clock_backend # Clock(), Clock().time_terminal_displaying(clk)
 
 
 @click.command()
-@click.option('--time', nargs=3, default=(0,0,0), help='The starting hour of the clock',type=int)
 @click.option('--format',default=24,  help='Time format (12h or 24h)', type=int)
-@click.option('--am', default=False, help='If format = 12, tell if time is starting AM or PM',type=bool)
-def clock(time, format, am):
-    hour, minute, second = time
+@click.option('--time', nargs=4, default=(0,0,0,0), help='The starting hour of the clock',type=int)
+def clock(time, format):
+    hour, minute, second, am = time
     if format == 12:
-        if am == 'am':
+        if am == 1:
             clk = clock_backend.Clock(hours=hour, minutes=minute, seconds=second, format24=False ,pm=False)
-        else:
+        elif am == 0:
             clk = clock_backend.Clock(hours=hour, minutes=minute, seconds=second, format24=False ,pm=True)
     else:
         clk = clock_backend.Clock(hours=hour, minutes=minute, seconds=second, format24=True ,pm=False)
-    clock_backend.time_terminal_displaying(clk)
+    clock_backend.time_terminal_displaying(time=clk,alarm=False)
 
 if __name__ == '__main__':
     clock()
