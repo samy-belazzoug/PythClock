59618ca871f57e0f77ebf04d67cbc276be2b1310
samy-belazzoug <samy.belazzoug@pm.me>
2026-09-05 23:56:35 +0200

    alarm cli supported

--- clock_cli.py
+++ clock_cli.py
@@ -3,14 +3,14 @@ import clock_backend # Clock(), Clock().time_terminal_displaying(clk)
 
 
 @click.command()
-@click.option('--format',default=24,  help='Time format (12h or 24h)', type=int)
-@click.option('--time', nargs=4, default=(0,0,0,0), help='The starting hour of the clock',type=int)
-@click.option('--alarm',nargs=5, default=(0,0,0,0,0), help='The alarm time wanted', type=int)
-def clock(time, format, alarm):
-    hour, minute, second, am = time
-    enabled_alarm, hour_alarm, minute_alarm, second_alarm, am_alarm = alarm
+@click.option('--time', nargs=5, default=(0,0,0,0,0), help='The starting hour of the clock',type=int)
+@click.option('--alarm',nargs=6, default=(0,0,0,0,0,0), help='The alarm time wanted', type=int)
+@click.option('--message',default="",help='If alarm, the message that will be displayed when its time',type=str)
+def clock(time, alarm, message):
+    hour, minute, second, format_clock, am = time
+    enabled_alarm, hour_alarm, minute_alarm, second_alarm, format_alarm, am_alarm = alarm
     if enabled_alarm == 0:
-        if format == 12:
+        if format_clock == 12:
             if am == 1:
                 clk = clock_backend.Clock(hours=hour, minutes=minute, seconds=second, format24=False ,pm=False)
             elif am == 0:
@@ -18,8 +18,23 @@ def clock(time, format, alarm):
         else:
             clk = clock_backend.Clock(hours=hour, minutes=minute, seconds=second, format24=True ,pm=False)
         return clock_backend.time_terminal_displaying(time=clk,alarm=False)
-    elif enabled_alarm == 1:
-        pass
 
+    elif enabled_alarm == 1:
+        if format_alarm == format_clock:
+            if format == 12:
+                if am == 1:
+                    clk = clock_backend.Clock(hours=hour, minutes=minute, seconds=second, format24=False ,pm=False)
+                elif am == 0:
+                    clk = clock_backend.Clock(hours=hour, minutes=minute, seconds=second, format24=False ,pm=True)
+
+                if am_alarm == 1:
+                    clk_alarm = clock_backend.Clock(hours=hour, minutes=minute, seconds=second, format24=False ,pm=False)
+                elif am_alarm == 0:
+                    clk_alarm = clock_backend.Clock(hours=hour, minutes=minute, seconds=second, format24=False ,pm=True)  
+            else:
+                clk = clock_backend.Clock(hours=hour, minutes=minute, seconds=second, format24=True ,pm=False)
+                clk_alarm = clock_backend.Clock(hours=hour_alarm, minutes=minute_alarm, seconds=second_alarm, format24=True ,pm=False)
+            return clock_backend.time_terminal_displaying(clk,True,clk_alarm, message)
+        
 if __name__ == '__main__':
     clock()
